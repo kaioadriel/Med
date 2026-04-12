716d22bfe1efc8b45d595ccfc7de14607c9ff7a4
Kaio Adriel <kaioadrielguimaraes@gmail.com>
2026-04-12 18:28:13 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE_NAME = 'med-app-cache-v4';
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then((cache) => {
-        return cache.addAll(['./', './index.html', './manifest.json', './med.png']);
-    }));
-});
-self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
-});
