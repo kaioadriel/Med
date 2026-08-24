9758a0a592a5679d17db609a8f19242d80911051
Kaio Adriel <kaioadrielguimaraes@gmail.com>
2026-08-24 10:32:29 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE = 'dosecerta-static-v1';
-const RUNTIME_CACHE = 'dosecerta-runtime-v1';
+const STATIC_CACHE = 'dosecerta-static-v2';
+const RUNTIME_CACHE = 'dosecerta-runtime-v2';
 
 const LOCAL_ASSETS = [
     './',
